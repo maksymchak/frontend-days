45c9bfbcbeaebfbaf1d9986b2229cee3e17932c6
maksymchak <maksym4ak@gmail.com>
2018-09-02 20:14:42 +0300

    add sliders

--- js/script.js
+++ js/script.js
@@ -14,4 +14,22 @@ function myFunction(ctx) {
   ctx.classList.toggle("change");
 }
 
+/**************** Sliders ****************/ 
+var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
 /****************  ****************/ 
